3a3ca89c2841b59e2654cc99b3fe8cf57a95176b
xieyonghong <18010623010@163.com>
2022-06-13 14:54:12 +0800

    feat: 添加DictSelect组件

---
+++ src/views/offer/Save/product/DictSelect.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-select v-bind="this.$props">
+    <el-option
+      v-for="item in dataSource"
+      :key="item.dictCode"
+      :value="item.dictValue"
+      :label="item.dictLabel"
+    />
+  </el-select>
+</template>
+
+<script setup name="DictSelect">
+import {onMounted, ref, watch} from "vue";
+import useDictionaryStore from "@/store/modules/dict"
+
+const dictionaryStore = useDictionaryStore();
+
+const props = defineProps({
+  dictType: {
+    type: String,
+    default: ''
+  }
+})
+
+const emits = defineEmits(['update'])
+
+const dataSource = ref([])
+
+const queryData = (value) => {
+  console.log('queryData',value)
+  if (value) {
+    dictionaryStore.getData(value).subscribe(data => {
+      dataSource.value = data || []
+    })
+  }
+}
+
+onMounted(() => {
+  queryData(props.dictType)
+})
+
+watch(() => props.dictType, (value) => {
+  console.log(value)
+  queryData(value)
+})
+
+</script>
+
+<style scoped>
+
+</style>
