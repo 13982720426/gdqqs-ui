33c457fb25332905288dd10bc3d022854cd8ac90
jackhoo_98 <jackhoo_98@foxmail.com>
2022-07-06 09:52:15 +0800

    feat：新增报价管理 页码

--- src/views/offer/index.vue
+++ src/views/offer/index.vue
@@ -4,10 +4,10 @@
       <div>
         <el-space>
           <el-button
-              type="primary"
-              icon="Plus"
-              v-hasPermi="['offer:add']"
-              @click="jumpAdd"
+            type="primary"
+            icon="Plus"
+            v-hasPermi="['offer:add']"
+            @click="jumpAdd"
           >
             新增
           </el-button>
@@ -15,64 +15,82 @@
       </div>
       <div>
         <el-space>
-          <el-input v-model="searchText" style="width: 200px;" placeholder="请输入报价单名称"/>
-          <el-button type="primary" @click="queryData(searchText)">查询</el-button>
+          <el-input
+            v-model="searchText"
+            style="width: 200px"
+            placeholder="请输入报价单名称"
+          />
+          <el-button type="primary" @click="queryData(searchText)">
+            查询
+          </el-button>
           <el-button @click="onReset()">重置</el-button>
         </el-space>
       </div>
     </div>
-    <el-table
-        :data="dataSource"
-        style="width: 100%"
-    >
-      <el-table-column prop="offerName" label="报价单名称"/>
-      <el-table-column prop="offerCode" label="编号" width="180"/>
-      <el-table-column prop="createTime" label="报价时间" width="160"/>
-      <el-table-column prop="customerName" label="客户信息" width="180"/>
-      <el-table-column prop="contractPrice" label="合同价格" width="180"/>
-      <el-table-column prop="profit" label="利润" width="180"/>
-      <el-table-column prop="createUserName" label="创建人" width="180"/>
+    <el-table :data="dataSource" style="width: 100%">
+      <el-table-column prop="offerName" label="报价单名称" />
+      <el-table-column prop="offerCode" label="编号" width="180" />
+      <el-table-column prop="createTime" label="报价时间" width="160" />
+      <el-table-column prop="customerName" label="客户信息" width="180" />
+      <el-table-column prop="contractPrice" label="合同价格" width="180" />
+      <el-table-column prop="profit" label="利润" width="180" />
+      <el-table-column prop="createUserName" label="创建人" width="180" />
       <el-table-column prop="name" label="操作" width="300" fixed="right">
-        <template #default="{row}">
+        <template #default="{ row }">
           <el-button @click="view(row.offerId)">详情查看</el-button>
           <el-button @click="edit(row.offerId)">编辑</el-button>
           <el-button>导出</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      v-model:page="params.pageNum"
+      v-model:limit="params.pageSize"
+      @pagination="queryData()"
+    />
   </div>
 </template>
 
 <script setup name="offer">
 import BaseTable from './BaseTable'
-import {queryList} from '@/api/offer'
-import {ref, onMounted} from 'vue'
-import {useRouter} from "vue-router";
-import {delOffer} from "../../api/business/offer";
+import { queryList } from '@/api/offer'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { delOffer } from '../../api/business/offer'
 import useOfferStore from '@/store/modules/offer'
 
 const offerStore = useOfferStore()
 
-const router = useRouter();
+const router = useRouter()
 const activeName = ref('All')
 const searchText = ref(undefined)
 const dataSource = ref([])
 
+const total = ref(0)
+const params = reactive({
+  pageNum: 1,
+  pageSize: 10,
+})
+
 const queryData = async (name) => {
-  const params = {}
-  console.log(name)
   if (name) {
     params.offerName = name
+  } else {
+    params.offerName = undefined
   }
   const resp = await queryList(params)
   if (resp.code === 200) {
+    console.log('resp', resp)
+    total.value = resp.total
     dataSource.value = resp.rows
   }
 }
 
 const jumpAdd = () => {
   offerStore.init()
-  router.push('/offer/save');
+  router.push('/offer/save')
 }
 
 const edit = (id) => {
@@ -80,9 +98,9 @@ const edit = (id) => {
     path: `/offer/save`,
     query: {
       id,
-      type: 'edit'
-    }
-  });
+      type: 'edit',
+    },
+  })
 }
 
 const view = (id) => {
@@ -90,9 +108,9 @@ const view = (id) => {
     path: `/offer/save`,
     query: {
       id,
-      type: 'view'
-    }
-  });
+      type: 'view',
+    },
+  })
 }
 
 const onReset = () => {
@@ -111,7 +129,6 @@ onMounted(() => {
   queryData()
   offerStore.init()
 })
-
 </script>
 
 <style lang="scss" scoped>
